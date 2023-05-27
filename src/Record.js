62b78b8385d43f76a8193fb9932344845cb83593
Shreedhar <urawane03@gmail.com>
2023-05-27 06:44:36 +0530

    add present count

--- src/Record.js
+++ src/Record.js
@@ -1,24 +1,28 @@
 import React, { useContext } from 'react'
 import { AppContext } from './App'
 export default function Record() {
-  let { rollNumbers , date } = useContext(AppContext)
+  let { rollNumbers, date } = useContext(AppContext)
 
-  const copyRollNos=()=>{
+  const copyRollNos = () => {
     const combined = `${date} - Attendance \n\n${rollNumbers}`
     navigator.clipboard.writeText(combined)
   }
 
-  let filteredNumbers = rollNumbers.sort((a,b)=>{
-    return a-b
-  }).join(" , ")
-  
+  let filteredNumbers = rollNumbers.sort((a, b) => {
+    return a - b
+  })
+
   return (
     <div className='my-8 flex flex-col gap-2'>
       <div name='attendance' className='max-w-[450px] w-full mx-auto p-3 border-2 border-blue-500 break-words'>
         <p>{date} <br />Attendance SE-A </p>
-        {filteredNumbers}
+        {filteredNumbers.map((ele,key)=>{
+          return <span key={key}>{ele}{`\t`}</span>
+        })}
       </div>
-      <button className='text-md bg-blue-500 text-white p-2' onClick={copyRollNos}>Copy Roll Nos</button>
+      <h2>Present Count : {filteredNumbers.length}</h2>
+      <button className='text-md bg-blue-500 text-white p-2' onClick={copyRollNos}>Copy Attendance</button>
     </div>
   )
 }
+
